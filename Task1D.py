06870651cbcef53c5d2ee081f50c796063f17193
Tian Ern <n.tianern@gmail.com>
2022-01-23 20:53:20 +0000

    Added Task 1D demonstration 2

--- Task1D.py
+++ Task1D.py
@@ -6,6 +6,10 @@ def run():
     Requirements for Task 1A
     Written by Tian Ern
     """
+
+    ############### DEMONSTRATION 1 ###############
+    print("***DEMONSTRATION 1: geo.rivers_with_station***")
+
     # Build a set of river names
     stations = build_station_list()
     rivers_set = rivers_with_station(stations)
@@ -16,8 +20,34 @@ def run():
     
     # Print output
     print("{} stations: First 10 - {}".format(len(rivers_list), rivers_list[:10]))
+    print("\n")
+    
+    ############### DEMONSTRATION 2 ###############
+    print("***DEMONSTRATION 2: geo.stations_by_river***")
+    rivers_to_stations_dict = stations_by_river(stations)
+    
+    # River Aire
+    stations_list = []
+    for station in rivers_to_stations_dict["River Aire"]:
+        stations_list.append(station.name)
+    stations_list.sort()
+    print("Stations located on River Aire: {}\n".format(stations_list))
+
+    # River Cam
+    stations_list = []
+    for station in rivers_to_stations_dict["River Cam"]:
+        stations_list.append(station.name)
+    stations_list.sort()
+    print("Stations located on River Cam: {}\n".format(stations_list))
+
+    # River Thames
+    stations_list = []
+    for station in rivers_to_stations_dict["River Thames"]:
+        stations_list.append(station.name)
+    stations_list.sort()
+    print("Stations located on River Thames: {}\n".format(stations_list))
 
 
 if __name__ == "__main__":
-    print("*** Task 1D: Tian Ern ***")
+    print("*** Task 1D: Tian Ern***")
     run()
