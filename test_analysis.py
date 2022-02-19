78890a325f87fa23f6e036c69b37b9df0ddf83fb
Tian Ern <n.tianern@gmail.com>
2022-02-19 22:24:41 +0000

    tests for Task 2F

--- test_analysis.py
+++ test_analysis.py
@@ -2,20 +2,26 @@
 Unit test for the analysis submodule
 """
 
-from floodsystem.stationdata import build_station_list
-from floodsystem.geo import rivers_with_station, stations_by_river, rivers_by_station_number
-from floodsystem.station import MonitoringStation
-from floodsystem.geo import stations_by_distance, stations_within_radius
+import numpy as np
+from datetime import timedelta
 
-def test_stations_by_distance():
+from pandas import array
+
+from floodsystem.analysis import polyfit
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.plot import plot_water_level_with_fit
+from floodsystem.flood import stations_highest_rel_level
+
+
+def test_polyfit():
     """
-    Task 1B by Rajiv
+    Task 2F by Tian Ern
     """
     stations = build_station_list()
-    x = stations_by_distance(stations, (52.2053, 0.1218))
-    assert isinstance(x, list)
-    for i in x:
-        assert isinstance(i, tuple)
-        assert len(i) == 2
-        assert isinstance(i[0], MonitoringStation)
-        assert isinstance(i[1], float)
+    update_water_levels(stations)
+    station = stations[0]
+    dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=2))
+    p_coeff = polyfit(dates, levels, 3)
+    assert isinstance(p_coeff, np.ndarray)
+    assert len(p_coeff) == 4
